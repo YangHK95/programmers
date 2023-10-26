b3c5c136a1a81321e9783289fd91a9023009fa04
YangHK95 <135406516+YangHK95@users.noreply.github.com>
2023-10-26 23:03:24 +0900

    [level unrated] Title: 문자열 돌리기, Time: 46.18 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    str2 = str.split('')
+    str2.forEach(e=>console.log(e))
+});
