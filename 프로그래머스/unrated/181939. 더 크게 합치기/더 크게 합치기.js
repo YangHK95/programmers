bb6406bff26d644da83c39a23eb5b4bf3894f1a9
YangHK95 <135406516+YangHK95@users.noreply.github.com>
2023-11-03 16:37:02 +0900

    [level unrated] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,9 +1,10 @@
 function solution(a, b) {
-   const answer = Number(`${a}${b}`)
-   const result = Number(`${b}${a}`)
-   if(answer>= result){
-       return answer
-   }else{
-       return result
-   }
+   // const answer = Number(`${a}${b}`)
+   // const result = Number(`${b}${a}`)
+   // if(answer>= result){
+   //     return answer
+   // }else{
+   //     return result
+   // }
+    return Math.max((Number(`${a}${b}`)),(Number(`${b}${a}`)))
 }
