47d130a33be698e90d3441bc0e26315c2cb1f3be
YangHK95 <135406516+YangHK95@users.noreply.github.com>
2023-11-20 18:26:11 +0900

    [level unrated] Title: 이어 붙인 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
@@ -1,14 +1,12 @@
 function solution(num_list) {
-    let str = num_list.toString()
     let odd = ""
     let even = ""
-    for( i = 0; i < str.length ; i++){
-       if(str[i] % 2 == 1){
-         odd += str[i] 
-       }
-       if(str[i] % 2 == 0) {
-          even += str[i]
-       }
+    for( i = 0; i < num_list.length ; i++){
+        if(num_list[i] % 2 === 0 ){
+          odd += num_list[i]
+        }else{
+         even += num_list[i]
+        }   
     }
-    return Number(odd) + Number(even)
+ return Number(odd) + Number(even)
 }
