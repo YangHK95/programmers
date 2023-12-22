1e76ff919f84469cdfb724a47b41b52a4064147b
YangHK95 <135406516+YangHK95@users.noreply.github.com>
2023-12-22 10:36:29 +0900

    [level unrated] Title: 수 조작하기 2, Time: 7.01 ms, Memory: 41.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
@@ -1,30 +1,11 @@
 function solution(numLog) {
-    let result = "";
-    let currentNum = numLog[0];
-
-    for (let i = 1; i < numLog.length; i++) {
-        const diff = numLog[i] - numLog[i - 1];
-
-        if (diff === 1) {
-            result += "w";
-            currentNum += 1;
-        } else if (diff === -1) {
-            result += "s";
-            currentNum -= 1;
-        } else if (diff === 10) {
-            result += "d";
-            currentNum += 10;
-        } else if (diff === -10) {
-            result += "a";
-            currentNum -= 10;
+    let str = []
+    for(i = 0 ; i< numLog.length ; i ++){
+        if(numLog[i]<numLog[i+1]){
+                str.push("w")
+            }
+        }if else(numLog[i]>numLog[i+1]){         
+                str.push("s")}
         }
-    }
-
-    return result;
+    return str
 }
-
-// 예제 입력
-const numLog = [0, 1, 0, 10, 0, 1, 0, 10, 0, -1, -2, -1];
-
-// 결과 출력
-console.log(solution(numLog)); // "wsdawsdassw"
